967d0fd16d417af0c1efb34d19130a87bd2f0064
Abdugapparova <kamosh20021108@gmail.com>
2022-09-28 22:11:54 +0600

    f commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -29,16 +29,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Article;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use App\Http\Resources\ArticleResource;
 
 /*
@@ -17,22 +18,20 @@ use App\Http\Resources\ArticleResource;
 |
 */
 
-Route::get('article/{id}',function($id){
-    return new ArticleResource(Article::findOrFail($id));
-
-});
-Route::get('/articles',function(){
-    return ArticleResource::collection(Article::all());
-
-});
+// routes of articles
+Route::get('article/{id}',function($id){return new ArticleResource(Article::findOrFail($id));});
+Route::get('/articles',function(){return ArticleResource::collection(Article::all());});
 Route::put('article/{id}',[ArticleController::class,'update']);
 Route::delete('article/{id}',[ArticleController::class,'destroy']);
 Route::post('/articles',[ArticleController::class,'store']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {return $request->user();});
 
+// routes of user (auth of user)
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
 
-    
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user',[AuthController::class,'user']);
+    Route::post('logout',[AuthController::class,'logout']);
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
